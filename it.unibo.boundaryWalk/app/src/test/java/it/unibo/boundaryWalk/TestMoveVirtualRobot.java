ecf5e78e4f43ff3e898f741d670bc7e0d7790710
LeoManto <leomantovani97@hotmail.it>
2021-02-28 16:43:36 +0100

    initial

---
+++ it.unibo.boundaryWalk/app/src/test/java/it/unibo/boundaryWalk/TestMoveVirtualRobot.java
@@ -0,0 +1,89 @@
+package it.unibo.boundaryWalk;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestMoveVirtualRobot {
+    private MoveVirtualRobot appl;
+
+    @Before
+    public void systemSetUp() {
+        System.out.println("TestMoveVirtualRobot | setUp: robot should be at HOME-DOWN ");
+        appl = new MoveVirtualRobot();
+    }
+
+    @After
+    public void  terminate() {
+        System.out.println("%%%  TestMoveVirtualRobot |  terminates ");
+    }
+
+    /*
+    @Test
+    public void testMovesNoFailing() {
+        System.out.println("TestMoveVirtualRobot | testWork ");
+        boolean moveFailed = appl.moveLeft(300);
+        assertTrue( ! moveFailed  );
+        moveFailed = appl.moveRight(1000);    //back to DOWN
+        assertTrue( ! moveFailed  );
+        moveFailed = appl.moveStop(100);
+        assertTrue( ! moveFailed  );
+    }
+
+    @Test
+    public void testMoveForwardNoHit() {
+        System.out.println("TestMoveVirtualRobot | testMoveForward ");
+        boolean moveFailed = appl.moveForward(400);
+        assertTrue( ! moveFailed  );
+        moveFailed = appl.moveBackward(400);  //back to home
+        assertTrue( ! moveFailed  );
+    }
+
+    @Test public void testMoveForwardHit() {
+        System.out.println("TestMoveVirtualRobot | testMoveForward ");
+        boolean moveFailed = appl.moveForward(1600);
+        assertTrue( moveFailed  );
+        moveFailed = appl.moveBackward(1600);       //back to home
+        assertTrue( moveFailed  );
+    }
+
+    @Test public void testBoundaryWalk(){
+        System.out.println("TestMoveVirtualRobot | BoundaryWalk");
+        String moves = "";
+        int i = 0;
+        while(i != 4){
+            boolean moveFailed = appl.moveForward(200);
+            moves += "w";
+            if(!moveFailed){
+                assertTrue(!moveFailed);
+            } else {
+                assertTrue(moveFailed);
+                moveFailed = appl.moveLeft(200);
+                assertTrue(!moveFailed);
+                moves += "l ";
+                i++;
+            }
+        }
+
+        System.out.println(moves);
+    }
+    */
+
+    @Test public void prova(){
+        boolean complete = appl.boundaryWalk(200);
+        assertTrue(complete);
+    }
+
+}
+/*
+See
+http://sqa.fyicenter.com/FAQ/JUnit/What_Is_JUnit_.html
+http://sqa.fyicenter.com/FAQ/JUnit/Can_You_Write_a_JUnit_Test_Case_Class_in_10_Minu.html
+http://sqa.fyicenter.com/FAQ/JUnit/Can_You_Explain_a_Sample_JUnit_Test_Case_Class_.html
+No main required
+public static void main(String[] args) {
+  junit.textui.TestRunner.run(DirListerTest.class);
+}
+ */
