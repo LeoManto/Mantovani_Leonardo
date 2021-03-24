d4c4068a90e72235fb6cb16fc8622825d06a82ea
LeoManto <leomantovani97@hotmail.it>
2021-03-24 18:18:06 +0100

    update

--- it.unibo.resumableBoundaryWalker/app/src/main/java/it/unibo/interaction/IssObserver.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.interaction;
-import org.json.JSONObject;
-
-public interface IssObserver {
-    public void handleInfo(String info);
-    public void handleInfo(JSONObject info);
-}
--- it.unibo.resumableBoundaryWalker/app/src/main/java/it/unibo/resumableBoundaryWalker/annotations/AppRobotSpec.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.resumableBoundaryWalker.annotations;
-
-import java.lang.annotation.*;
-
-@Target(value = { ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.TYPE   })
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-public @interface AppRobotSpec {
-}
