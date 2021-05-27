a22881e2b3e61bb7058231daefe8efd7408373b7
LeoManto <leomantovani97@hotmail.it>
2021-05-27 17:27:45 +0200

    Update

--- it.unibo.raspberry21/it.unibo.rasp2021/sonaronrasp.pl
+++ it.unibo.raspberry21/it.unibo.rasp2021/sonaronrasp.pl
@@ -1,8 +1,8 @@
 %====================================================================================
 % sonaronrasp description   
 %====================================================================================
-mqttBroker("broker.hivemq.com", "1883", "sonar/data").
-context(ctxsonaronrasp, "192.168.178.75",  "TCP", "8028").
+mqttBroker("localhost", "1883", "sonar/data").
+context(ctxsonaronrasp, "localhost",  "TCP", "8068").
  qactor( sonarsimulator, ctxsonaronrasp, "sonarSimulator").
   qactor( sonardatasource, ctxsonaronrasp, "sonarHCSR04Support2021").
   qactor( datacleaner, ctxsonaronrasp, "dataCleaner").
