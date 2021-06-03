52e335c18f04cd61774d811ff6a5cd2738535dda
LeoManto <leomantovani97@hotmail.it>
2021-06-03 09:43:18 +0200

    Update

--- it.unibo.raspberry21/it.unibo.sonarAsResource/sonarresource.pl
+++ it.unibo.raspberry21/it.unibo.sonarAsResource/sonarresource.pl
@@ -1,7 +1,7 @@
 %====================================================================================
 % sonarresource description   
 %====================================================================================
-mqttBroker("192.168.178.21", "1883", "sonar/data").
+mqttBroker("localhost", "1883", "sonar/data").
 context(ctxsonarresource, "localhost",  "TCP", "8028").
  qactor( sonarresource, ctxsonarresource, "it.unibo.sonarresource.Sonarresource").
   qactor( sendermock, ctxsonarresource, "it.unibo.sendermock.Sendermock").
