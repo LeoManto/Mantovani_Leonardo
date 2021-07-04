e7ab537e9b869e9385f2371a13434606554ce0d7
LeoManto <leomantovani97@hotmail.it>
2021-07-04 17:46:28 +0200

    Update

--- AutomatedCarParking/sprint_1/it.unibo.ParkManagerService_s1/parkingservice.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% parkingservice description   
-%====================================================================================
-context(ctxparkingservice, "localhost",  "TCP", "8052").
- qactor( outsonar, ctxparkingservice, "sonarSimulator").
-  qactor( weightsensor, ctxparkingservice, "weightsensorSimulator").
-  qactor( client, ctxparkingservice, "it.unibo.client.Client").
-  qactor( parkingmanagerservice, ctxparkingservice, "it.unibo.parkingmanagerservice.Parkingmanagerservice").
