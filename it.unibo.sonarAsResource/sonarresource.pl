da1b412ab592eff829f47f6a94ca18f895345a82
Leonardo Mantovani <50448215+LeoManto@users.noreply.github.com>
2021-09-18 11:49:12 +0200

    Update

--- it.unibo.sonarAsResource/sonarresource.pl
+++ it.unibo.sonarAsResource/sonarresource.pl
@@ -1,7 +1,7 @@
 %====================================================================================
 % sonarresource description   
 %====================================================================================
-mqttBroker("localhost", "1883", "sonar/data").
+mqttBroker("broker.hivemq.com", "1883", "parkingArea/feedback").
 context(ctxsonarresource, "localhost",  "TCP", "8028").
  qactor( sonarresource, ctxsonarresource, "it.unibo.sonarresource.Sonarresource").
   qactor( appllogic, ctxsonarresource, "it.unibo.appllogic.Appllogic").
